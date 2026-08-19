98ccce932c48b92695d63e2bde1ae9db69e52390
Vandan17 <vandanp1705@gmail.com>
2026-08-20 00:24:25 +0530

    post

--- post-app/backend/src/controllers/post.controller.js
+++ post-app/backend/src/controllers/post.controller.js
@@ -1,9 +1,43 @@
-const getPosts = (req,res) => {
-    res.status(200).json({
-        message: "Posts fetched successfully"
-    });
+const Post = require("../models/post.model");
+
+const getPosts = async (req,res) => {
+    try {
+        const posts = await Post.find().sort({ createdAt: -1 });
+
+        res.status(200).json({
+        message: "Posts fetched successfully",
+        posts
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: "failed to fetch posts",
+            error: error.message
+        });
+    }
+};
+
+const createPost = async (req, res) => {
+    try {
+        const { caption, image } = req.body;
+
+        const post = await Post.create({
+            caption,
+            image
+        });
+
+        res.status(201).json({
+            message: "Post created successfully",
+            post
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: "Failed to create post",
+            error: error.message
+        });
+    }
 };
 
 module.exports = {
-    getPosts
+    getPosts,
+    createPost
 };
--- post-app/backend/src/routes/post.routes.js
+++ post-app/backend/src/routes/post.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { getPosts } = require("../controllers/post.controller");
+const { getPosts,createPost } = require("../controllers/post.controller");
 
 const router = express.Router();
 
 router.get("/", getPosts);
+router.post("/", createPost);
 
 module.exports = router;
